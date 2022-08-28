72c0047192b7c55396a7dd370e14f33ec1104ba5
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-08-28 15:19:45 -0300

    Classe user

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.api;
 
+import com.example.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+
+		User user = new User(1, "Vladmir", "vlad@gmail.com", "1");
 	}
 
 }
---
+++ src/main/java/com/example/api/domain/User.java
@@ -0,0 +1,25 @@
+package com.example.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
+}
