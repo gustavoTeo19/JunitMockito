e67bccc0feabfa70337c101934d3e15f79a7d3fb
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-09-03 19:24:59 -0300

    teste  para a classe principal

--- src/main/java/com/example/api/ApiApplication.java
+++ src/main/java/com/example/api/ApiApplication.java
@@ -9,8 +9,6 @@ public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-		User user = new User(1, "Vladmir", "vlad@gmail.com", "1");
 	}
 
 }
