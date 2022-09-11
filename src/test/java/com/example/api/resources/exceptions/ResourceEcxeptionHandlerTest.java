07037ae1870d8e61cf2e8a0d2714ba37749ae028
GustavoTeo <62210677+GustavoTeo@users.noreply.github.com>
2022-09-10 21:37:17 -0300

    testes para manipulador de excecoes metodo data integrity

--- src/test/java/com/example/api/resources/exceptions/ResourceEcxeptionHandlerTest.java
+++ src/test/java/com/example/api/resources/exceptions/ResourceEcxeptionHandlerTest.java
@@ -1,5 +1,6 @@
 package com.example.api.resources.exceptions;
 
+import com.example.api.services.exceptions.DataIntegratyViolationException;
 import com.example.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,9 +35,20 @@ class ResourceEcxeptionHandlerTest {
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals("Objeto não encontrado", response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
     }
 
     @Test
-    void dataIntegratyViolationException() {
+    void dataIntegrityViolationException() {
+        ResponseEntity<StandardError> response = ecxeptionHandler
+                .dataIntegrityViolationException(new DataIntegratyViolationException("Email já cadastrado"), new MockHttpServletRequest());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals("Email já cadastrado", response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
